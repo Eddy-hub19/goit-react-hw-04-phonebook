d76ce2476341eeed82728aa1b377aea99e4065d4
EduardSekan <sekaneduard@gmail.com>
2023-01-31 17:02:44 +0200

    chnages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Container, SubTitle, Title } from './App.styled';
-// import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
